b8ee8d18f670c6435f3fcc29008917668701695e
ts6075 <75653667+ts6075@users.noreply.github.com>
2022-05-17 18:35:45 +0800

    修改為MVC專案

---
+++ NC_H_FISC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NC_H_FISC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
