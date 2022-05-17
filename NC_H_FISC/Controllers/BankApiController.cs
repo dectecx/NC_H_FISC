d52f8efb8a98891e8c72e465ebee92d314038e44
ts6075 <75653667+ts6075@users.noreply.github.com>
2022-05-17 20:44:24 +0800

    調整ModelResult

--- NC_H_FISC/Controllers/BankApiController.cs
+++ NC_H_FISC/Controllers/BankApiController.cs
@@ -21,85 +21,134 @@ namespace NC_H_FISC.Controllers
         [HttpPost]
         public ModelResult<InsertModelReq, InsertModelRsp> Insert(InsertModelReq req)
         {
-            db.Banktab.Add(new Banktab
+            try
             {
-                Bankcode = req.bankCode,
-                Bankname = req.bankName,
-                Telzone = req.telZone,
-                Telno = req.telNo,
-                Updatedate = DateTime.Now.ToString("yyyyMMdd")
-            });
-            db.SaveChanges();
-            return new ModelResult<InsertModelReq, InsertModelRsp> { Req = req, Rsp = new InsertModelRsp() };
+                db.Banktab.Add(new Banktab
+                {
+                    Bankcode = req.bankCode,
+                    Bankname = req.bankName,
+                    Telzone = req.telZone,
+                    Telno = req.telNo,
+                    Updatedate = DateTime.Now.ToString("yyyyMMdd")
+                });
+                db.SaveChanges();
+                return BuildResponse<InsertModelReq, InsertModelRsp>(true, null, req, new InsertModelRsp());
+            }
+            catch (Exception ex)
+            {
+                return BuildResponse<InsertModelReq, InsertModelRsp>(false, ex.Message, null, null);
+            }
         }
 
         [Route("api/v1/fisc/bank/{bankCode}")]
         [HttpGet]
         public ModelResult<GetModelReq, GetModelRsp> Get(GetModelReq req)
         {
-            var model = db.Banktab.Where(e => e.Bankcode == req.bankCode).FirstOrDefault();
-            var result = new GetModelRsp
+            try
             {
-                bankCode = model.Bankcode,
-                bankName = model.Bankname,
-                telZone = model.Telzone,
-                telNo = model.Telno
-            };
-            return new ModelResult<GetModelReq, GetModelRsp> { Req = null, Rsp = result };
+                var model = db.Banktab.Where(e => e.Bankcode == req.bankCode).FirstOrDefault();
+                var result = new GetModelRsp
+                {
+                    bankCode = model.Bankcode,
+                    bankName = model.Bankname,
+                    telZone = model.Telzone,
+                    telNo = model.Telno
+                };
+                return BuildResponse<GetModelReq, GetModelRsp>(true, null, null, result);
+            }
+            catch (Exception ex)
+            {
+                return BuildResponse<GetModelReq, GetModelRsp>(false, ex.Message, null, null);
+            }
         }
 
         [Route("api/v1/fisc/bank")]
         [HttpGet]
         public ModelResult<GetManyModelReq, GetManyModelRsp> GetMany(GetManyModelReq req)
         {
-            var models = db.Banktab.AsQueryable();
-            if (!string.IsNullOrWhiteSpace(req.bankCode))
+            try
             {
-                models = models.Where(e => e.Bankcode == req.bankCode);
-            }
-            if (!string.IsNullOrWhiteSpace(req.telZone))
-            {
-                models = models.Where(e => e.Telzone == req.telZone);
-            }
-            var result = new GetManyModelRsp
-            {
-                data = new List<GetManyModelData>()
-            };
-            foreach(var item in models)
-            {
-                result.data.Add(new GetManyModelData
+                var models = db.Banktab.AsQueryable();
+                if (!string.IsNullOrWhiteSpace(req.bankCode))
                 {
-                    bankCode = item.Bankcode,
-                    bankName = item.Bankname,
-                    telZone = item.Telzone,
-                    telNo = item.Telno
-                });
+                    models = models.Where(e => e.Bankcode == req.bankCode);
+                }
+                if (!string.IsNullOrWhiteSpace(req.telZone))
+                {
+                    models = models.Where(e => e.Telzone == req.telZone);
+                }
+                var result = new GetManyModelRsp
+                {
+                    data = new List<GetManyModelData>()
+                };
+                foreach (var item in models)
+                {
+                    result.data.Add(new GetManyModelData
+                    {
+                        bankCode = item.Bankcode,
+                        bankName = item.Bankname,
+                        telZone = item.Telzone,
+                        telNo = item.Telno
+                    });
+                }
+                return BuildResponse<GetManyModelReq, GetManyModelRsp>(true, null, null, result);
             }
-            return new ModelResult<GetManyModelReq, GetManyModelRsp> { Req = null, Rsp = result };
+            catch (Exception ex)
+            {
+                return BuildResponse<GetManyModelReq, GetManyModelRsp>(false, ex.Message, null, null);
+            }
         }
 
         [Route("api/v1/fisc/bank/{bankCode}")]
         [HttpDelete]
         public ModelResult<DeleteModelReq, DeleteModelRsp> Delete(DeleteModelReq req)
         {
-            var model = db.Banktab.Where(e => e.Bankcode == req.bankCode).FirstOrDefault();
-            db.Banktab.Remove(model);
-            db.SaveChanges();
-            return new ModelResult<DeleteModelReq, DeleteModelRsp> { Req = null, Rsp = new DeleteModelRsp() };
+            try
+            {
+                var model = db.Banktab.Where(e => e.Bankcode == req.bankCode).FirstOrDefault();
+                db.Banktab.Remove(model);
+                db.SaveChanges();
+                return BuildResponse<DeleteModelReq, DeleteModelRsp>(true, null, null, new DeleteModelRsp());
+            }
+            catch (Exception ex)
+            {
+                return BuildResponse<DeleteModelReq, DeleteModelRsp>(false, ex.Message, null, null);
+            }
         }
 
         [Route("api/v1/fisc/bank/{bankCode}")]
         [HttpPut]
         public ModelResult<UpdateModelReq, UpdateModelRsp> Update(UpdateModelReq req)
         {
-            var model = db.Banktab.Where(e => e.Bankcode == req.bankCode).FirstOrDefault();
-            model.Bankcode = req.bankCode;
-            model.Bankname = req.bankName;
-            model.Telzone = req.telZone;
-            model.Telno = req.telNo;
-            model.Updatedate = DateTime.Now.ToString("yyyyMMdd");
-            db.SaveChanges();
-            return new ModelResult<UpdateModelReq, UpdateModelRsp> { Req = req, Rsp = new UpdateModelRsp() };
+            try
+            {
+                var model = db.Banktab.Where(e => e.Bankcode == req.bankCode).FirstOrDefault();
+                model.Bankcode = req.bankCode;
+                model.Bankname = req.bankName;
+                model.Telzone = req.telZone;
+                model.Telno = req.telNo;
+                model.Updatedate = DateTime.Now.ToString("yyyyMMdd");
+                db.SaveChanges();
+                return BuildResponse<UpdateModelReq, UpdateModelRsp>(true, null, req, new UpdateModelRsp());
+            }
+            catch (Exception ex)
+            {
+                return BuildResponse<UpdateModelReq, UpdateModelRsp>(false, ex.Message, null, null);
+            }
+        }
+
+        private ModelResult<TReq, TRsp> BuildResponse<TReq, TRsp>(bool success, string errorMsg, TReq req, TRsp rsp)
+        {
+            return new ModelResult<TReq, TRsp>
+            {
+                Success = success,
+                ErrorMessage = errorMsg,
+                Data = new ModelData<TReq, TRsp>
+                {
+                    Req = req,
+                    Rsp = rsp
+                }
+            };
         }
     }
 }
--- NC_H_FISC/Models/ModelResult.cs
+++ NC_H_FISC/Models/ModelResult.cs
@@ -2,6 +2,13 @@
 {
     public class ModelResult<TReq, TRsp>
     {
+        public bool Success { get; set; }
+        public string ErrorMessage { get; set; }
+        public ModelData<TReq, TRsp> Data { get; set; }
+    }
+
+    public class ModelData<TReq, TRsp>
+    {
         public TReq Req { get; set; }
         public TRsp Rsp { get; set; }
     }
